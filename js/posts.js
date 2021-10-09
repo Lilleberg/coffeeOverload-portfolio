c9615442c1acd88a30d613ef030142f8ba629a48
Maria Lilleberg Holm <maria.lilleberg@gmail.com>
2021-10-10 01:01:54 +0200

    added category filter

--- js/posts.js
+++ js/posts.js
@@ -1,26 +1,32 @@
 const main = document.querySelector("main");
 const postContainer = document.querySelector(".main-section");
 const viewMoreButton = document.querySelector(".view-more");
+const buttonCategory = document.querySelector(".category");
+const buttonPopular = document.querySelector(".popular");
+const buttonUseful = document.querySelector(".useful");
+const buttonFacts = document.querySelector(".facts");
+const buttonEducational = document.querySelector(".educational");
+const buttonInteresting = document.querySelector(".interesting");
+const buttonAllPosts = document.querySelector(".view-all");
 
 let url = "https://gamehub-maria.digital/projectexam1/wp-json/wp/v2/posts?_embed&per_page=12";
 
-var counter = 0;
 async function getPosts(url) {
   try {
     const response = await fetch(url);
     const posts = await response.json();
 
-    for (let i = 0; i < posts.length; i++) {
-      if (i <= 9) createHTML(posts[i], postContainer);
-    }
+    postContainer.innerHTML = " ";
+    for (let i = 0; i < posts.length && i <= 9; i++) {
+      createHTML(posts[i], postContainer);
 
-    viewMoreButton.onclick = function () {
-      for (let j = 10; j < posts.length; j++) {
-        createHTML(posts[j], postContainer);
+      viewMoreButton.onclick = function () {
+        for (i = 10; i < posts.length; i++) {
+          createHTML(posts[i], postContainer);
+        }
+        viewMoreButton.style.display = "none";
       }
-      viewMoreButton.style.display = "none";
     }
-
   } catch (error) {
     console.log("ERROR: ", error);
     setErrorMessage(main);
@@ -29,8 +35,39 @@ async function getPosts(url) {
 
 getPosts(url);
 
-function createHTML(post, container) {
-  container.innerHTML += `
+const popularUrl = "https://gamehub-maria.digital/projectexam1/wp-json/wp/v2/posts?_embed&categories=2";
+const factsUrl = "https://gamehub-maria.digital/projectexam1/wp-json/wp/v2/posts?_embed&categories=3";
+const usefulUrl = "https://gamehub-maria.digital/projectexam1/wp-json/wp/v2/posts?_embed&categories=4";
+const educationalUrl = "https://gamehub-maria.digital/projectexam1/wp-json/wp/v2/posts?_embed&categories=5"
+const interestingUrl = "https://gamehub-maria.digital/projectexam1/wp-json/wp/v2/posts?_embed&categories=6";
+
+async function getCategories(url, button) {
+  try {
+    const response = await fetch(url);
+    const posts = await response.json();
+    button.onclick = function () {
+
+      postContainer.innerHTML = " ";
+      for (let i = 0; i < posts.length; i++) {
+        createHTML(posts[i]);
+        viewMoreButton.style.display = "none";
+      }
+    }
+
+  } catch (error) {
+    console.log("ERROR", error);
+  }
+}
+
+getCategories(popularUrl, buttonPopular);
+getCategories(factsUrl, buttonFacts);
+getCategories(usefulUrl, buttonUseful);
+getCategories(educationalUrl, buttonEducational);
+getCategories(interestingUrl, buttonInteresting);
+getCategories(url, buttonAllPosts);
+
+function createHTML(post) {
+  postContainer.innerHTML += `
   <div class="post-container">
     <a href="post_specific.html?id=${post.id}">
       <h2 class="post-heading">${post.title.rendered}</h2>
@@ -38,4 +75,14 @@ function createHTML(post, container) {
       ${post.excerpt.rendered}
     </a>
   </div>`;
+}
+
+const dispCategory = document.querySelector(".disp-sort-content");
+
+buttonCategory.onclick = function () {
+  if (dispCategory.style.display === "block") {
+    dispCategory.style.display = "none";
+  } else {
+    dispCategory.style.display = "block";
+  }
 }
